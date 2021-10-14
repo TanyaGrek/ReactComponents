8dff164fe849e76434ddbabaf9e4b619fb1de297
Tetiana Grek <tetiana_grek@epam.com>
2021-10-14 12:55:11 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+import path, { dirname } from 'path';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const isProduction = process.env.NODE_ENV === 'production';
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+const config = {
+  mode: isProduction ? 'production' : 'development',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        loader: 'file-loader',
+        options: { name: 'src/asset/[name].[ext]' }
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    open: true,
+    host: 'localhost',
+    port: 4000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/assets/htmlTemplate.html',
+    }),
+  ],
+};
+
+if (isProduction) {
+  config.plugins.push(new MiniCssExtractPlugin());
+}
+
+export default config;
